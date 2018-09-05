e9b2163a4ce6ea85cd80feab886d8b19e71663d6
Dawson Mortenson <dawson.mortenson@gmail.com>
2018-09-05 15:29:33 -0700

    create a GenServer to handle channel IO for mapped brightness values

--- phx_kiosk/lib/phx_kiosk_web/channels/home_channel.ex
+++ phx_kiosk/lib/phx_kiosk_web/channels/home_channel.ex
@@ -3,12 +3,25 @@ defmodule PhxKioskWeb.HomeChannel do
 
   require Logger
 
+  @from_range 1..100
+  @to_range 15..255
+
   def join("home:lobby", payload, socket) do
     if authorized?(payload) do
+      send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
+  end
+
+  def handle_in("brightness", %{"value" => value} = payload, socket) do
+    Logger.debug("Brightness: #{inspect value}")
+    broadcast socket, "brightness", payload
+
+    map_range(@from_range, @to_range, value)
+    |> PhxKiosk.Backlight.set_brightness()
+    {:noreply, socket}
   end
 
   # Channels can be used in a request/response fashion
@@ -24,11 +37,15 @@ defmodule PhxKioskWeb.HomeChannel do
     {:noreply, socket}
   end
 
-  def handle_in("brightness", %{"value" => value} = payload, socket) do
-     Logger.debug("Brightness: #{inspect value}")
-     broadcast socket, "brightness", payload
+  def handle_info(:after_join, socket) do
+    brightness =
+      map_range(@to_range, @from_range, PhxKiosk.Backlight.brightness())
+    push(socket, "brightness", %{value: brightness})
+    {:noreply, socket}
+  end
 
-     {:noreply, socket}
+  defp map_range(a1 .. a2, b1 .. b2, s) do
+    b1 + (s - a1) * (b2 - b1) / (a2 - a1)
   end
 
   # Add authorization logic here as required.
